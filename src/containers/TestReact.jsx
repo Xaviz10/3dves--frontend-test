51b8730478c867dadd908ab3c0747f3000ea1d36
Javier A. Gamboa D <xaviz2007@hotmail.com>
2021-06-25 20:27:40 -0500

    React Configuration Verification

---
+++ src/containers/TestReact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const TestReact = () => {
+    return (
+        <h1>Hola Mundo</h1>
+    );
+}
+
+export default TestReact;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestReact from './containers/TestReact';
+
+
+
+ReactDOM.render(<TestReact />, document.getElementById('root'));
