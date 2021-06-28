0c7b15f6931356af30ff918ff698ce6f3de40f8d
Javier A. Gamboa D <xaviz2007@hotmail.com>
2021-06-27 22:52:33 -0500

    Final Table style

--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -25,80 +25,95 @@ const PokemonTable = () => {
     
         return (
             <div className="pokemon-table-container">
-                <div className="pokemon-table-grid">
-                    <div className="pokemon-table__header">
-                        <h1 className="pokemon-preview__id">Id</h1>
-                        <h2 className="pokemon-preview__name">Name</h2>
-                        <h3 className="pokemon-type__item-1">Type 1</h3>
-                        <h3 className="pokemon-type__item-2">Type 2</h3>
-                        <h3 className="pokemon-ability__item-1">Ability 1</h3>
-                        <h3 className="pokemon-ability__item-2">Ability 2</h3>
-                    </div>
+                <table className="pokemon-table">
+                    <tr className="pokemon-table__header">
+                        <td className="pokemon-preview__id ">Id</td>
+                        <td className="pokemon-preview__name ">Name</td>
+                        <td className="pokemon-type__item ">Type 1</td>
+                        <td className="pokemon-type__item ">Type 2</td>
+                        <td className="pokemon-preview_hp">HP</td>
+                        <td className="pokemon-preview_atk">ATK</td>
+                        <td className="pokemon-preview_df">DF</td>
+                        <td className="pokemon-preview_satk">S-ATK</td>
+                        <td className="pokemon-preview_sdf">S-DF</td>
+                        <td className="pokemon-preview_spd">SPD</td>
+                        <td className="pokemon-ability__item ">Ability 1</td>
+                        <td className="pokemon-ability__item ">Ability 2</td>
+                    </tr>
+                    
                     {data.pokemons.map(item => {
                         return (
-                            <div key={item.id} className="pokemon-table__row"
+                            <tr key={item.id} className="pokemon-table__row"
                                 onClick={() => hadleUpdateCard(item.id)}>
-                                <h1 className="pokemon-preview__id">#{item.id}</h1>
-                                <h2 className="pokemon-preview__name">{capitalizeFirstLetter(item.name)}</h2>
+                                <td className="pokemon-preview__id pokemon-table__row">#{item.id}</td>
+                                <td className="pokemon-preview__name pokemon-table__row">{capitalizeFirstLetter(item.name)}</td>
                                 {item.types.map(typeItem => {
                                     return (
-                                        <h3 key={typeItem.type.name} className="pokemon-type__item">{capitalizeFirstLetter(typeItem.type.name)}</h3>
+                                        <td key={typeItem.type.name} className="pokemon-type__item pokemon-table__row">{capitalizeFirstLetter(typeItem.type.name)}</td>
                                     );
                                 }
                                 )}
 
-                                {item.types.map(abilityItem => {
+                                <td className="pokemon-preview_hp">{item.stats[0].base_stat }</td>
+                                <td className="pokemon-preview_atk">{item.stats[1].base_stat }</td>
+                                <td className="pokemon-preview_df">{item.stats[2].base_stat }</td>
+                                <td className="pokemon-preview_satk">{item.stats[3].base_stat }</td>
+                                <td className="pokemon-preview_sdf">{item.stats[4].base_stat }</td>
+                                <td className="pokemon-preview_spd">{item.stats[5].base_stat }</td>
+
+                                {item.abilities.map(abilityItem => {
                                     return (
-                                        <h3 key={abilityItem.type.name} className="pokemon-ability__item">{capitalizeFirstLetter(abilityItem.type.name)}</h3>
+                                        <td key={abilityItem.ability.name} className="pokemon-ability__item ">{capitalizeFirstLetter(abilityItem.ability.name)}</td>
                                     );
                                 }
                                 )}
 
-                            </div>
+                            </tr>
                         );
                     }
                     )}
-                </div>
+                </table>
                 <UpdatePokemons />
             </div>
         );
     } else {
         return (
             <div className="pokemon-table-container">
-                <div className="pokemon-table-grid">
-                    <div className="pokemon-table__row pokemon-table__header">
-                        <h1 className="pokemon-preview__id">Id</h1>
-                        <h2 className="pokemon-preview__name">Name</h2>
-                        <h3 className="pokemon-type__item-1">Type 1</h3>
-                        <h3 className="pokemon-type__item-2">Type 2</h3>
-                        <h3 className="pokemon-ability__item-1">Ability 1</h3>
-                        <h3 className="pokemon-ability__item-2">Ability 2</h3>
-                    </div>
+                <table className="pokemon-table-grid">
+                    <tr className="pokemon-table__header">
+                        <td className="pokemon-preview__id ">Id</td>
+                        <td className="pokemon-preview__name ">Name</td>
+                        <td className="pokemon-type__item-1 ">Type 1</td>
+                        <td className="pokemon-type__item-2 ">Type 2</td>
+                        <td className="pokemon-ability__item-1 ">Ability 1</td>
+                        <td className="pokemon-ability__item-2 ">Ability 2</td>
+                    </tr>
+                    
                     {data.pokemons.map(item => {
                         return (
-                            <div key={item.id} className="pokemon-table__row"
+                            <tr key={item.id} className="pokemon-table__row"
                                 onClick={() => hadleUpdateCard(item.id)}>
-                                <h1 className="pokemon-preview__id">#{item.id}</h1>
-                                <h2 className="pokemon-preview__name">{capitalizeFirstLetter(item.name)}</h2>
+                                <td className="pokemon-preview__id">#{item.id}</td>
+                                <td className="pokemon-preview__name">{capitalizeFirstLetter(item.name)}</td>
                                 {item.types.map(typeItem => {
                                     return (
-                                        <h3 key={typeItem.type.name} className="pokemon-type__item">{capitalizeFirstLetter(typeItem.type.name)}</h3>
+                                        <td key={typeItem.type.name} className="pokemon-type__item">{capitalizeFirstLetter(typeItem.type.name)}</td>
                                     );
                                 }
                                 )}
 
-                                {item.types.map(abilityItem => {
+                                {item.abilities.map(abilityItem => {
                                     return (
-                                        <h3 key={abilityItem.type.name} className="pokemon-ability__item">{capitalizeFirstLetter(abilityItem.type.name)}</h3>
+                                        <td key={abilityItem.ability.name} className="pokemon-ability__item ">{capitalizeFirstLetter(abilityItem.ability.name)}</td>
                                     );
                                 }
                                 )}
 
-                            </div>
+                            </tr>
                         );
                     }
                     )}
-                </div>
+                </table>
                 <UpdatePokemons />
             </div>
         );
